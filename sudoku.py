73c85d2da81e237dc258582d85b993f2df02d326
jossnock <144459459+jossnock@users.noreply.github.com>
2023-12-14 21:26:29 +0000

    dnf

--- sudoku.py
+++ sudoku.py
@@ -9,20 +9,7 @@ sudoku_board = [[None, None, None, None, None, None, None, None, None],
                 [None, None, None, None, None, None, None, None, None]]
 
 def sudoku_player(sudoku_board):
-    def sudoku_finish_checker(sudoku_board):
-        solved = False
-        for i in range(len(sudoku_board)):
-            if sudoku_board[i] == [None, None, None, None, None, None, None, None, None]:
-                solved = False
-            else:
-                for j in range(len(sudoku_board[i])):
-                    if sudoku_board[i][j] == None:
-                        solved = False
-                    else:
-                        solved = True
-        return solved
-
-    while sudoku_finish_checker(sudoku_board) == False: #while it's unsolved
+    while unsolved_positions > 0: #while it's unsolved
         def sudoku_print(sudoku_board):
             #prints the board neatly
             pass
@@ -35,17 +22,19 @@ def sudoku_player(sudoku_board):
             ##return position
 
         #asking for position and digit:
-        current_position = algebraic_notation_converter(input('Position a digit\n'))
-        sudoku_board[current_position[1]][current_position[0]] = 'X' #shows position
-        sudoku_print(sudoku_board)
+        def digit_placer(position, digit):
+            digit_placed = False
+            while digit_placed == False:
+                current_position = algebraic_notation_converter(input('Position a digit\n'))
+                sudoku_board[current_position[1]][current_position[0]] = 'X' #shows position
+
+                
+                sudoku_print(sudoku_board)
 
 
-        current_digit = input("Which digit? Or type 'x' to redo position.\n")
-        while current_digit == 'x':
-            ##current_position = 
-            pass
-    
         sudoku_board[current_position[1]][current_position[0]] = current_digit
+        unsolved_positions += 1
+        
         sudoku_print(sudoku_board)
 
 
