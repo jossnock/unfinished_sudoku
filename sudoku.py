0d036382f45ac27727e1f2316f38e113a6c22563
jossnock <144459459+jossnock@users.noreply.github.com>
2023-12-14 16:33:24 +0000

    algebra

--- sudoku.py
+++ sudoku.py
@@ -12,14 +12,28 @@ def sudoku_player(sudoku_board):
     def sudoku_print(sudoku_board):
         pass
     sudoku_print(sudoku_board)
-    current_position = input('Position a digit')
     #first part corresponds to column, second for mark
-    current_position_column = 
-    current_position_row = 
+    def algebraic_notation_converter(algebraic_position):
+        pass
+    current_position = algebraic_notation_converter(input('Position a digit'))
+
+    current_position_column = current_position[0]
+    current_position_row = current_position[1]
     current_digit = input('Which number?')
     sudoku_board[current_position_row][current_position_column] = current_digit
     
 
+
+    
+
+
+
+
+
+
+
+
+    
 """
 Todo:
  - make UI (maybe add row/column numbers/letters)
