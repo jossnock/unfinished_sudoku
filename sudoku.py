f7f04d574082bfce7bd3254f2132d812dde90fb2
jossnock <144459459+jossnock@users.noreply.github.com>
2023-12-14 22:12:26 +0000

    all but sudoku_print and converter (entire thing still untested)

--- sudoku.py
+++ sudoku.py
@@ -19,26 +19,50 @@ def sudoku_player(sudoku_board):
         def algebraic_notation_converter(algebraic_position): 
             #converts algebraic notation into a position on the grid e.g. a7 becomes 12
             pass
+            #checks if its a valid input
             ##return position
 
+
         #asking for position and digit:
-        def digit_placer(position, digit):
-            digit_placed = False
-            while digit_placed == False:
-                current_position = algebraic_notation_converter(input('Position a digit\n'))
-                sudoku_board[current_position[1]][current_position[0]] = 'X' #shows position
+        digit_position_confirmed = False
+        while digit_position_confirmed == False:
+            current_position = algebraic_notation_converter(input("Position a digit: "))
+            sudoku_board[current_position[1]][current_position[0]] = 'X' #marks position to confirm that the player got the correct position
+            sudoku_print(sudoku_board)
+            
+            confirmation = input("Is this the correct position? (y/n)\n").lower()
+            if confirmation == 'y':
+                digit_position_confirmed = True
+            elif confirmation == 'n':
+                continue
+            else:
+                print("Invalid input. Please enter 'y' for yes or 'n' for no.")
+        
 
+        digit_value_valid = False
+        while digit_value_valid == False:
+            try:
+                current_digit = input('Choose a digit between 0 and 9:')
                 
-                sudoku_print(sudoku_board)
+                if current_digit.isdigit():
+                    current_digit = int(current_digit)
+                    if 0 <= current_digit <= 9:
+                        digit_value_valid = True
+                    else:
+                        print('Input is not between 0 and 9.')
+                else:
+                    print('Invalid input. Please enter a valid integer.')
 
+            except ValueError:
+                print('Invalid input. Please enter a digit between 0 and 9.')
 
         sudoku_board[current_position[1]][current_position[0]] = current_digit
         unsolved_positions += 1
-        
+
         sudoku_print(sudoku_board)
+        
+    print("Congradulations! You've completed this Sudoku puzzle.")
 
-
-        sudoku_finish_checker(sudoku_board)
 
 
 
