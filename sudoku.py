cb2f03324e2b89bb8a06b3d18eac1eab373a1639
jossnock <144459459+jossnock@users.noreply.github.com>
2023-12-14 16:24:09 +0000

    positioning typed in

--- sudoku.py
+++ sudoku.py
@@ -12,11 +12,13 @@ def sudoku_player(sudoku_board):
     def sudoku_print(sudoku_board):
         pass
     sudoku_print(sudoku_board)
-    current_osition = input('Position a digit')
+    current_position = input('Position a digit')
     #first part corresponds to column, second for mark
+    current_position_column = 
+    current_position_row = 
     current_digit = input('Which number?')
+    sudoku_board[current_position_row][current_position_column] = current_digit
     
-
 
 """
 Todo:
