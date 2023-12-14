972848fb66d4c364c267ee19f6dc24b045636669
jossnock <144459459+jossnock@users.noreply.github.com>
2023-12-14 21:12:46 +0000

    routine save

--- sudoku.py
+++ sudoku.py
@@ -1,24 +1,36 @@
-sudoku_board = [['','','','','','','','','']
-               ,['','','','','','','','','']
-               ,['','','','','','','','','']
-               ,['','','','','','','','','']
-               ,['','','','','','','','','']
-               ,['','','','','','','','','']
-               ,['','','','','','','','','']
-               ,['','','','','','','','','']
-               ,['','','','','','','','','']]
+sudoku_board = [[None, None, None, None, None, None, None, None, None]
+               ,[None, None, None, None, None, None, None, None, None]
+               ,[None, None, None, None, None, None, None, None, None]
+               ,[None, None, None, None, None, None, None, None, None]
+               ,[None, None, None, None, None, None, None, None, None]
+               ,[None, None, None, None, None, None, None, None, None]
+               ,[None, None, None, None, None, None, None, None, None]
+               ,[None, None, None, None, None, None, None, None, None]
+               ,[None, None, None, None, None, None, None, None, None]]
 
 def sudoku_player(sudoku_board):
-    while sudoku_board.count('') > 0: #while there's empty spaces
+    def sudoku_finish_checker(sudoku_board):
+        solved = False
+        for i in range(len(sudoku_board)):
+            if sudoku_board[i] == [None, None, None, None, None, None, None, None, None]:
+                solved = False
+            else:
+                for j in range(len(sudoku_board[i])):
+                    if sudoku_board[i][j] == None:
+                        solved = False
+        return solved
+
+    while sudoku_finish_checker(sudoku_board) == False: #while it's unsolved
         def sudoku_print(sudoku_board):
             #prints the board neatly
             pass
+            print(sudoku_board)
         sudoku_print(sudoku_board)
 
         def algebraic_notation_converter(algebraic_position): 
             #converts algebraic notation into a position on the grid e.g. a7 becomes 12
             pass
-            return position
+            ##return position
 
         #asking for position and digit:
         current_position = algebraic_notation_converter(input('Position a digit\n'))
@@ -28,18 +40,19 @@ def sudoku_player(sudoku_board):
 
         current_digit = input("Which digit? Or type 'x' to redo position.\n")
         while current_digit == 'x':
-            current_position = 
+            ##current_position = 
             pass
     
         sudoku_board[current_position[1]][current_position[0]] = current_digit
         sudoku_print(sudoku_board)
 
 
+        sudoku_finish_checker(sudoku_board)
 
 
 
 
-
+sudoku_player(sudoku_board)
 
     
 """
