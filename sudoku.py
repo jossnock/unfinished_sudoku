ef03e9eaf21657c45d1e796bf8491e503c9f0a75
jossnock <144459459+jossnock@users.noreply.github.com>
2023-12-14 16:17:40 +0000

    structure!!

--- sudoku.py
+++ sudoku.py
@@ -8,12 +8,14 @@ sudoku_board = [['','','','','','','','','']
                ,['','','','','','','','','']
                ,['','','','','','','','','']]
 
-#def sudoku_player(sudoku_board):
-
-
-
-
-
+def sudoku_player(sudoku_board):
+    def sudoku_print(sudoku_board):
+        pass
+    sudoku_print(sudoku_board)
+    current_osition = input('Position a digit')
+    #first part corresponds to column, second for mark
+    current_digit = input('Which number?')
+    
 
 
 """
