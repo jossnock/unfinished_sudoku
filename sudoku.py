d72cfbf360de7466dc58874ad6d9d3f44676c12b
jossnock <144459459+jossnock@users.noreply.github.com>
2023-12-14 15:43:04 +0000

    first steps

---
+++ sudoku.py
@@ -0,0 +1,26 @@
+sudoku_board = [['','','','','','','','','']
+               ,['','','','','','','','','']
+               ,['','','','','','','','',''],
+                ['','','','','','','','','']
+               ,['','','','','','','','','']
+               ,['','','','','','','','',''],
+                ['','','','','','','','','']
+               ,['','','','','','','','','']
+               ,['','','','','','','','','']]
+
+#def sudoku_player(sudoku_board):
+
+
+
+
+
+
+
+"""
+Todo:
+ - make UI (maybe add row/column numbers/letters)
+ - implement number placing
+ - make notes
+
+ - use % to find which coords are in the boxes
+"""
